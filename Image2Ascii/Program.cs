d4da5b7a357df49ed3414a910096c5581e8d49a0
Richard Chin <richardchinloy@hotmail.com>
2017-10-07 11:52:19 -0700

    First working version

---
+++ Image2Ascii/Program.cs
@@ -0,0 +1,40 @@
+﻿/*************************************************************************
+ * Author: Richard Chin
+ * Date: October 2017
+ * Description:
+ *      ASCII art generator using a given bitmap as source.
+ *************************************************************************/
+
+
+using System;
+
+namespace Image2Ascii
+{
+    public static class AAOptions
+    {
+    }
+
+    class Program
+    {
+        // >Image2Ascii <bitmapfilename>
+        static void Main(string[] args)
+        {
+
+            if(args.Length < 1)
+            {
+                Console.WriteLine("Please supply the bitmap file path");
+                return;
+            }
+
+            ConvertBitmapToASCII(args[0]);
+
+        }
+
+
+        
+        static void ConvertBitmapToASCII(string filename)
+        {
+        }
+
+    }
+}
