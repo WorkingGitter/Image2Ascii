f681a15abb106f3b02805bf3a9b1b6a1de50173c
Richard Chin <richardchinloy@hotmail.com>
2017-10-31 15:47:50 +0000

    Now accept http image links

--- Image2Ascii/Program.cs
+++ Image2Ascii/Program.cs
@@ -10,6 +10,7 @@ using System.Drawing;
 using System.IO;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 
 namespace Image2Ascii
 {
@@ -29,13 +30,14 @@ namespace Image2Ascii
             long rt = 0;
             foreach(var s in args)
             {
+                /****** Future Option ********
                 if(String.Compare(s, "-Ts", true) == 0)
                 {
                     rt |= (long)ASCIIRenderer.RenderOptions.rtAlgSimple;
                     continue;
-                }
+                }*/
 
-                if(String.Compare(s, "-D", true) == 0)
+                if(String.Compare(s, "-d", true) == 0)
                 {
                     rt |= (long)ASCIIRenderer.RenderOptions.rtShowWtdChrs;
                     continue;
@@ -47,15 +49,16 @@ namespace Image2Ascii
                     continue;
                 }
 
-                if (String.Compare(s.Substring(0, 2), "-I", true) == 0)
+                if (String.Compare(s.Substring(0, 2), "-i", true) == 0)
                 {
                     filename = s.Substring(2, s.Length - 2);
                     continue;
                 }
             }
 
+            /****** TBD
             if (rt == 0)
-                rt = (long)ASCIIRenderer.RenderOptions.rtAlgSimple;
+                rt = (long)ASCIIRenderer.RenderOptions.rtAlgSimple; */
 
             if (String.IsNullOrEmpty(filename))
             {
@@ -68,13 +71,19 @@ namespace Image2Ascii
             aRender.RenderToConsole();
         }
 
+
+        /**************************************************************************
+        * Display help screen if no paramters given
+        ***************************************************************************/
         private static void DisplayHelp()
         {
             Console.WriteLine("Usage:");
-            Console.WriteLine("  AArt -I<image filename> [options]");
+            Console.WriteLine("  AArt -i<image filename> [options]");
             Console.WriteLine();
             Console.WriteLine("options:");
-            Console.WriteLine(" -Ts Simple conversion (simple char set)");
+            Console.WriteLine(" -i<image filename> path and filename to the image. Also accepts web links.");
+            Console.WriteLine("   (BMP, GIF, JPG, PNG or TIFF)");
+            /*Console.WriteLine(" -Ts Simple conversion (simple char set)");*/ // future option
             Console.WriteLine(" -g  Generate and use weighted characters");
             Console.WriteLine(" -d  Display weighted characters");
             return;
@@ -90,15 +99,15 @@ namespace Image2Ascii
     {
         public enum RenderOptions
         {
-            rtAlgSimple        = 1,
+            rtAlgSimple        = 1,         // future option...not used
             rtShowWtdChrs   = 1 << 1,
             rtWtdChrs  = 1 << 2
-            
-
         }
 
         private string _imagepath = "";
         private long _options = (long)RenderOptions.rtAlgSimple;
+
+        // Default rendering characters
         private List<char> _CharTable = new List<char>()
                                                 { ' ', '.', '-', ';', 'o', '?', 'b', '*', '%', 'X', '#', '@' };
 
@@ -137,11 +146,14 @@ namespace Image2Ascii
                 BuildCharacterWeighting(ref _CharTable);
             }
 
+            /********* For future options
             if ((_options & (long)RenderOptions.rtAlgSimple) > 0)
             {
                 ConvertUsingSimpleMethod(_imagepath);
                 return;
-            }
+            }*/
+
+            ConvertUsingSimpleMethod(_imagepath);
         }
 
 
@@ -156,78 +168,103 @@ namespace Image2Ascii
         * An output size of 80 characters is used.
         * A vertical adjustment of 2.2 is used so that the final output looks
         * proportional.
+        * 
+        * PARAMETERS:
+        *   filename - This can be either the full pathname to a local file (BMP)
         ***************************************************************************/
         private void ConvertUsingSimpleMethod(string filename)
         {
             if (String.IsNullOrEmpty(filename))
                 return;
 
+            Bitmap bitmap           = null;
+            string filename_lower   = filename.ToLower();
+            bool isWebsource        = filename.Contains("http");
+
             try
             {
-                using (Bitmap bitmap = new Bitmap(filename))
+                if (isWebsource)
+                {   // get stream from web source
+                    WebClient client = new WebClient();
+                    Stream webstream = client.OpenRead(filename_lower);
+                    bitmap = new Bitmap(webstream);
+                    webstream.Dispose();
+                }
+                else
                 {
-                    // Calculate the scaling adjustment we will use. 
-                    // Note that we are only doing this for the width. The height will be automatically
-                    // done.
-                    double scale = bitmap.Width / 80;
-                    if (scale < 1.0f)
-                        scale = 1.0f;
+                    bitmap = new Bitmap(filename_lower);
+                }
 
-                    int y = 0;      // image Y-axis to sample
-                    int x = 0;      // image X-axis to sample
-                    int yC = 0;     // current console row
-                    int xC = 0;     // current console column
+                // Calculate the scaling adjustment we will use. 
+                // Note that we are only doing this for the width. The height will be automatically
+                // done.
+                double scale = bitmap.Width / 80;
+                if (scale < 1.0f)
+                    scale = 1.0f;
 
-                    while (y < bitmap.Height)
+                int y = 0;      // image Y-axis to sample
+                int x = 0;      // image X-axis to sample
+                int yC = 0;     // current console row
+                int xC = 0;     // current console column
+
+                while (y < bitmap.Height)
+                {
+                    for (xC = 0; xC < 80; xC++)
                     {
-                        for (xC = 0; xC < 80; xC++)
-                        {
-                            // calculate the image X-axis to sample.
-                            // We are doing a straight scaling.
-                            x = (int)Math.Round(xC * scale, MidpointRounding.AwayFromZero);
+                        // calculate the image X-axis to sample.
+                        // We are doing a straight scaling.
+                        x = (int)Math.Round(xC * scale, MidpointRounding.AwayFromZero);
 
-                            // Adjust if we fall outside the image boundaries
-                            if (x < 0)
-                                x = 0;
+                        // Adjust if we fall outside the image boundaries
+                        if (x < 0)
+                            x = 0;
 
-                            if (x >= bitmap.Width)
-                                x = bitmap.Width - 1;
+                        if (x >= bitmap.Width)
+                            x = bitmap.Width - 1;
 
-                            Color pixel = bitmap.GetPixel(x, y);
+                        Color pixel = bitmap.GetPixel(x, y);
 
-                            // Get the brightness of this pixel.
-                            // This ranges from 0.0 to 1.0
-                            float brightness = pixel.GetBrightness();
+                        // Get the brightness of this pixel.
+                        // This ranges from 0.0 to 1.0
+                        float brightness = pixel.GetBrightness();
 
-                            // map this to our available character lookup table
-                            int index = (int)Math.Round((brightness * (_CharTable.Count - 1)), MidpointRounding.AwayFromZero);
-                            Debug.Assert(index >= 0);
-                            Debug.Assert(index < _CharTable.Count);
+                        // map this to our available character lookup table
+                        int index = (int)Math.Round((brightness * (_CharTable.Count - 1)), MidpointRounding.AwayFromZero);
+                        Debug.Assert(index >= 0);
+                        Debug.Assert(index < _CharTable.Count);
 
-                            // Write our character to screen
-                            Console.Write(_CharTable[index]);
-                        }
-                        Console.WriteLine();
-
-                        // increment row, then map to the equivalent Y-axis in our image
-                        // Note, I am ajusting the ratio due to the console font used.
-                        // A factor of 2.2 seem to produce the best result to elimitate 
-                        // image distortion.
-                        yC++;
-                        y = (int)Math.Round(yC * scale * 2.2, MidpointRounding.AwayFromZero);
+                        // Write our character to screen
+                        Console.Write(_CharTable[index]);
                     }
+                    Console.WriteLine();
 
-                } // using (Bitmap bitmap = new Bitmap(filename))
+                    // increment row, then map to the equivalent Y-axis in our image
+                    // Note, I am ajusting the ratio due to the console font used.
+                    // A factor of 2.2 seem to produce the best result to elimitate 
+                    // image distortion.
+                    yC++;
+                    y = (int)Math.Round(yC * scale * 2.2, MidpointRounding.AwayFromZero);
+                }
 
             }
             catch (FileNotFoundException)
             {
                 Console.WriteLine("Unable to open file");
             }
-            catch(System.ArgumentException)
+            catch (System.ArgumentException)
             {
                 Console.WriteLine("Invalid image input");
             }
+            catch(WebException wex)
+            {
+                Console.WriteLine($"{wex.Message}");
+            }
+            finally
+            {
+                if(bitmap != null)
+                    bitmap.Dispose();
+            }
+
         } // private void ConvertUsingSimpleMethod(string filename)
 
 
